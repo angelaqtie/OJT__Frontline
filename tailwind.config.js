00f0d076c46f3fe5cab765a93def80e668f1e46a
Lornz18 <mellgodwinbarza22@gmail.com>
2024-10-18 13:35:39 +0800

    frontline

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#008542",
+        secondary: "#d9a925",
+        blue: "#3B5897",
+      },
+      fontFamily: {
+        raleway: "Raleway",
+      }
+    },
+  },
+  plugins: [],
+};
+
